137d0e62f664075627c99e6db2991b34095f8065
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-04-30 18:26:15 +0200

    Removed swagger parts and changed names of controller methods.

--- Presentation/Controllers/EventRulesController.cs
+++ Presentation/Controllers/EventRulesController.cs
@@ -12,39 +12,39 @@ namespace Presentation.Controllers
         private readonly IForbiddenItemService _forbiddenItemService = forbiddenItemService;
 
         [HttpPost]
-        public async Task<IActionResult> AddRules(AddRulesRequest addRulesRequest)
+        public async Task<IActionResult> Create(AddRulesRequest addRulesRequest)
         {
             if (!ModelState.IsValid) { return BadRequest(); }
 
             var addRulesForm = FormFactory.Create(addRulesRequest);
             var result = await _forbiddenItemService.AddAForbiddenItem(addRulesForm);
 
-            return result.Success ? Ok(result) : BadRequest();
+            return result.Success ? Ok(result) : BadRequest(result);
         }
 
         [HttpPut]
-        public async Task<IActionResult> UpdateRules(UpdateRulesRequest updateRulesRequest)
+        public async Task<IActionResult> Update(UpdateRulesRequest updateRulesRequest)
         {
             if (!ModelState.IsValid) { return BadRequest(); }
 
             var updateRulesForm = FormFactory.Create(updateRulesRequest);
             var result = await _forbiddenItemService.UpdateForbiddenItems(updateRulesForm);
 
-            return result.Success ? Ok(result) : BadRequest();
+            return result.Success ? Ok(result) : BadRequest(result);
         }
 
-        [HttpGet("/id")]
-        public async Task<IActionResult> GetAllRules(string id)
+        [HttpGet("/{id}")]
+        public async Task<IActionResult> GetRulesForId(string id)
         {
             var result = await _forbiddenItemService.GetAllForbiddenItemsById(id);
-            return result.Success ? Ok(result) : BadRequest();
+            return result.Success ? Ok(result) : BadRequest(result);
         }
 
-        [HttpDelete("/id")]
-        public async Task<IActionResult> RemoveRules(string id)
+        [HttpDelete("/{id}")]
+        public async Task<IActionResult> Delete(string id)
         {
             var result = await _forbiddenItemService.RemoveForbiddenItems(id);
-            return result.Success ? Ok(result) : BadRequest();
+            return result.Success ? Ok(result) : BadRequest(result);
         }
     }
 }
