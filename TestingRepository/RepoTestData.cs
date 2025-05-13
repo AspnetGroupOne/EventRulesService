81b32f00231c3737d3594d94f44d3baf70f26d43
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-05-13 10:09:22 +0200

    Moved the test data.

---
+++ TestingRepository/RepoTestData.cs
@@ -0,0 +1,65 @@
+﻿using Core.Domain.Entities;
+
+namespace TestingRepository;
+
+public class RepoTestData
+{
+    public static readonly ForbiddenEntity[] ValidForbiddenEntities = {
+        new ForbiddenEntity
+        {
+            EventId = "1",
+            Alcohol = true,
+            Bike = false,
+            Camera = false,
+            Hazard = false,
+            Knife = false,
+            Merch = false,
+            Noise = false,
+            Pets = false,
+            Picnic = false,
+            Pill = false,
+            Tent = false,
+            Umbrella = false
+        },
+        new ForbiddenEntity
+        {
+            EventId = "2",
+            Alcohol = false,
+            Bike = false,
+            Camera = false,
+            Hazard = false,
+            Knife = false,
+            Merch = false,
+            Noise = false,
+            Pets = false,
+            Picnic = false,
+            Pill = false,
+            Tent = false,
+            Umbrella = false
+        }
+    };
+    public static readonly ForbiddenEntity[] InvalidForbiddenEntities = {
+        new ForbiddenEntity
+        {
+            EventId = "1",
+        },
+        new ForbiddenEntity
+        {
+            EventId = "6767676",
+            Alcohol = false,
+            Bike = false,
+            Camera = false,
+            Hazard = false,
+            Knife = false,
+            Merch = false,
+            Noise = false,
+            Pets = false,
+            Picnic = false,
+            Pill = false,
+            Tent = false,
+            Umbrella = false
+        }
+    };
+
+
+}
---
+++ TestingServices/ServTestData.cs
@@ -0,0 +1,76 @@
+﻿using Core.Domain.Entities;
+using Core.Domain.Models;
+
+namespace TestingServices;
+
+public class ServTestData
+{
+    public static readonly ForbiddenEntity[] ValidForbiddenEntities = {
+        new ForbiddenEntity
+        {
+            EventId = "1",
+            Alcohol = true,
+            Bike = false,
+            Camera = false,
+            Hazard = false,
+            Knife = false,
+            Merch = false,
+            Noise = false,
+            Pets = false,
+            Picnic = false,
+            Pill = false,
+            Tent = false,
+            Umbrella = false
+        }
+    };
+    public static readonly AddRulesForm[] ValidForbiddenAddForm = {
+        new AddRulesForm
+        {
+            EventId = "1",
+            Alcohol = true,
+            Bike = false,
+            Camera = false,
+            Hazard = false,
+            Knife = false,
+            Merch = false,
+            Noise = false,
+            Pets = false,
+            Picnic = false,
+            Pill = false,
+            Tent = false,
+            Umbrella = false
+        }
+    };
+    public static readonly UpdateRulesForm[] ValidUpdateForbiddenAddForm = {
+        new UpdateRulesForm
+        {
+            EventId = "1",
+            Alcohol = true,
+            Bike = false,
+            Camera = false,
+            Hazard = false,
+            Knife = false,
+            Merch = false,
+            Noise = false,
+            Pets = false,
+            Picnic = false,
+            Pill = false,
+            Tent = false,
+            Umbrella = false
+        }
+    };
+    public static readonly AddRulesForm[] InvalidForbiddenAddForm = {
+        new AddRulesForm
+        {
+            EventId = "1",
+        },
+        new AddRulesForm
+        {
+            EventId = "2",
+            Alcohol = false,
+            Bike = false,
+            Camera = false,
+            Hazard = false,
+        }
+    };
+}
