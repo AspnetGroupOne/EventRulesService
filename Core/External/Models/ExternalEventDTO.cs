9510414fdcf4869cebd7a36c6b6c53f1ef9b7e7d
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-06-01 18:21:42 +0200

    Added swagger documentation and comments.

--- Core/External/Models/ExternalEventDTO.cs
+++ Core/External/Models/ExternalEventDTO.cs
@@ -2,6 +2,8 @@
 
 public class ExternalEventDTO
 {
+    // Learned about POCOs/DTOs from chatgpt. It is basically a model shaped the same way as the model in the other api
+    // into which you load and hold the data for the moment you need it.
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public DateTime Date { get; set; }
